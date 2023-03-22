7c5905a55aa91b2c46d99a4e94bcb35ee904715b
frotan <frotan1954@gmail.com>
2023-03-22 14:17:59 +0430

    advance jav01

---
+++ src/thread/day01/MultiThreading02.java
@@ -0,0 +1,22 @@
+package thread.day01;
+
+public class MultiThreading02 {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("-------Application with Thread is started------------------------");
+         Printer printer = new Printer();
+
+         MyThread myThread = new MyThread(printer);
+         YourThread yourThread = new YourThread(printer);
+          myThread.start();
+        //  myThread.join();// your thread wait for MyThread to finish print documents
+         // this is the first way to synchronize the thread .
+         yourThread.start();
+        // yourThread.run();///Note: If you call run() method you just call the run method it will not start the thread
+         yourThread.join();
+         System.out.println("Application  with thread is finished");
+
+    }
+
+
+}
---
+++ src/thread/day01/Printer.java
@@ -0,0 +1,23 @@
+package thread.day01;
+
+public class Printer {
+
+   void printDocuments(int numOfCopies,String docName){
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        for (int i = 1; i <=numOfCopies; i++) {
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(">> Print "+docName +" "+i);
+
+        }
+
+    }
+}
